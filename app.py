6d57be6a754189ff80a331f304760a9251a51b06
Ashlesha Daulanpure <ashleshadaulanpure@gmail.com>
2026-08-17 12:35:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,6 +127,7 @@ def create_app():
     return app
 
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run(host="127.0.0.1", port=5000, debug=True)
+app = create_app()
+
+     if __name__ == "__main__":
+         app.run(host="127.0.0.1", port=5000, debug=True)
